43140e15931b97f2c752a51cb8f5df502d33cc6b
Aaron <aarongraybill3@gmail.com>
2021-12-21 23:49:45 -0600

    added search for song in queue for jump

--- commands/jump.js
+++ commands/jump.js
@@ -1,15 +1,50 @@
 //checks if serverQueue exists if the queue length is > 0 and if the song specified extist if it does serverQueue.jump is set to the specified int
 const { MessageEmbed } = require('discord.js');
-const {deleteMsg, leave} = require('../modules');
+const {deleteMsg, distance, topResult} = require('../modules');
 
 module.exports = {
     name:'jump',
     description: 'jumps to the specified song',
     async jump(message, args, serverQueue, serverDisconnectIdle){
         var i = parseInt(args);
+        var query = args.join(' ');
         if(serverQueue){
             if(serverQueue.loopsong === false){
                 if(serverQueue.songs.length > 1){
+                    if (isNaN(i)) {
+                        const options = [];
+                        let proceed = true;
+                        let arr = serverQueue.shuffle ? [...serverQueue.shuffledSongs] : [...serverQueue.songs];
+                        for (let j = 0;
+                            j < arr.length;
+                            j++) {
+                                const bool = arr[j].title.toLowerCase().includes(query);
+                                if (bool) {
+                                    i = arr.map(video => video.title).indexOf(arr[j].title);
+                                    proceed = false;
+                                    break;
+                                }
+                                else {
+                                    let dif = distance(query, arr[j].title);
+                                    options.push({dif: dif, song: arr[j]});
+                                }
+                            }
+                        // todo improve algorithm
+                        // if (proceed) {
+                        //     const result = topResult(options); 
+                        //     i = arr.map(song => song.title).indexOf(result);
+                        // }
+                        if (proceed) {
+                            const noMatch = new MessageEmbed() 
+                                .setColor('RED')
+                                .setDescription(':rofl: No Exact Matches Found Please Check Your Spelling')
+                            ;
+                            message.channel.send({embeds: [noMatch]})
+                                .then(msg => deleteMsg(msg, 30000, false))
+                            ;
+                            return;
+                        }
+                    }
                     if(serverQueue.shuffle === false){
                         if(serverQueue.songs[i]){
                             serverQueue.jump = i;
--- modules.js
+++ modules.js
@@ -1,47 +1,93 @@
 const { getVoiceConnection } = require("@discordjs/voice");
-module.exports =  { 
-    async deleteMsg(msg, time, bool) {
-        if (bool) {
-        
-        }
-        else {
-            setTimeout( async () => { await msg.delete() }, time);
-        }
-    }, 
-    async leave(q, di, msg) {
-        const id = msg.guild.id
-        const vc = getVoiceConnection(id);
-        const sdi = di.get(id);
-        const sq = q.get(id);
-        if (vc) {
-            if (sq) {
-                if (sq.nowPlaying) {
-                    sq.nowPlaying.delete();
+
+const distance = (name = '', possibleVid = '') => {
+    const track = Array(possibleVid.length + 1)
+      .fill(null)
+      .map(() => Array(name.length + 1).fill(null));
+    for (let i = 0; i <= name.length; i += 1) {
+      track[0][i] = i;
+    }
+    for (let j = 0; j <= possibleVid.length; j += 1) {
+      track[j][0] = j;
+    }
+    for (let j = 1; j <= possibleVid.length; j += 1) {
+      for (let i = 1; i <= name.length; i += 1) {
+        const indicator = name[i - 1] === possibleVid[j - 1] ? 0 : 1;
+        track[j][i] = Math.min(
+          track[j][i - 1] + 1,
+          track[j - 1][i] + 1,
+          track[j - 1][i - 1] + indicator
+        );
+      }
+    }
+    return track[possibleVid.length][name.length];
+}
+
+const topResult = (arr) => {
+    let v = 0;
+    let j = 0;
+    for (i = 0;
+        i < arr.length;
+        i++) {
+            if (v === 0) {
+                v = arr[i].dif;
+                j = i;
+            }  
+            else {
+                if (v > arr[i].dif) {
+                    v = arr[i].dif;
+                    j = i;
                 }
+        }
+    }
+    return arr[j].song.title;
+}
+
+async function deleteMsg(msg, time, bool) {
+    if (bool) {
+    
+    }
+    else {
+        setTimeout( async () => { await msg.delete() }, time);
+    }
+}
+
+
+async function leave(q, di, msg) {
+    const id = msg.guild.id
+    const vc = getVoiceConnection(id);
+    const sdi = di.get(id);
+    const sq = q.get(id);
+    if (vc) {
+        if (sq) {
+            if (sq.nowPlaying) {
+                sq.nowPlaying.delete();
             }
-            if (sdi.msgs[0]) {
-                for (let i = 0;
-                    i < sdi.msgs.length;
-                    i++) {
-                        sdi.msgs[i].delete();
-                    }
-            }
-            if (sdi.queueMsgs[0]) {
-                for (let i = 0;
-                    i < sdi.queueMsgs.length;
-                    i++) {
-                        sdi.queueMsgs[i].delete();
-                    }
-            }
-            if (vc) {
-                vc.disconnect()
-            }
-            if (sq) {
-                q.delete(id);
-            }
-            if (sdi) {
-                di.delete(id);
-            }
+        }
+        if (sdi.msgs[0]) {
+            for (let i = 0;
+                i < sdi.msgs.length;
+                i++) {
+                    sdi.msgs[i].delete();
+                }
+        }
+        if (sdi.queueMsgs[0]) {
+            for (let i = 0;
+                i < sdi.queueMsgs.length;
+                i++) {
+                    sdi.queueMsgs[i].delete();
+                }
+        }
+        if (vc) {
+            vc.disconnect()
+        }
+        if (sq) {
+            q.delete(id);
+        }
+        if (sdi) {
+            di.delete(id);
         }
     }
 }
+
+module.exports =  { deleteMsg, leave, distance, topResult };
