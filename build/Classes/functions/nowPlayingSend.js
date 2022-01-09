98cd882c3c1229e2fb1cae7b18cfd3b25f9f361f
Aaron <aarongraybill3@gmail.com>
2022-01-09 14:08:23 -0600

    delete nowPlaying on new nowPlaying request

--- build/Classes/functions/nowPlayingSend.js
+++ build/Classes/functions/nowPlayingSend.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
+const modules_1 = require("../../modules/modules");
+const maps_1 = require("../../maps");
 async function nowPlayingSend() {
     const serverQueue = this;
+    const { DisconnectIdle } = (0, maps_1.default)();
     if (serverQueue.currentsong[0]) {
+        if (serverQueue.nowPlaying) {
+            await (0, modules_1.deleteMsg)(serverQueue.nowPlaying, 0, DisconnectIdle.get(1));
+            serverQueue.nowPlaying = undefined;
+        }
         const msg = serverQueue.message.channel.send({ embeds: [new discord_js_1.MessageEmbed()
                     .setColor('#0099ff')
                     .setTitle(`:thumbsup: Now Playing`)
--- typescript/Classes/functions/nowPlayingSend.ts
+++ typescript/Classes/functions/nowPlayingSend.ts
@@ -1,9 +1,16 @@
 import {MessageEmbed} from 'discord.js';
+import { deleteMsg } from '../../modules/modules';
 import Queue from '../Queue';
+import getMaps from '../../maps';
 
 export default async function nowPlayingSend() {
     const serverQueue: Queue = this;
+    const {DisconnectIdle} = getMaps();
     if (serverQueue.currentsong[0]) {
+        if (serverQueue.nowPlaying) {
+            await deleteMsg(serverQueue.nowPlaying, 0, DisconnectIdle.get(1));
+            serverQueue.nowPlaying = undefined
+        }
         const msg = serverQueue.message.channel.send({embeds: [new MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`:thumbsup: Now Playing`)
