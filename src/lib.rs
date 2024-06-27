78c03b3f2c0694b07d2b5c9b6d45d1238cd1aa4d
Aaron <aarongraybill3@gmail.com>
2024-06-27 00:10:13 -0500

    fix: VC_DC_TIMEOUT_IN_SEC to 30 min

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub type SmError = Box<dyn std::error::Error + Send + Sync>;
 pub type SmContext<'a> = poise::Context<'a, UserData, SmError>;
 pub type CommandResult = Result<(), SmError>;
 
-pub const VC_DC_TIMEOUT_IN_SEC: u64 = 5;
+pub const VC_DC_TIMEOUT_IN_SEC: u64 = 1800;
 
 #[derive(Debug, Default)]
 pub struct SmMsg {
